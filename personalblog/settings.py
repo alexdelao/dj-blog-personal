2c0e00081b17f917f90bd07382b155976c581fd9
paletalex <misopitas@gmail.com>
2019-12-16 05:03:22 -0600

    Configurando el settings

--- personalblog/settings.py
+++ personalblog/settings.py
@@ -138,8 +138,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'misopitas@gmail.com'
-EMAIL_HOST_PASSWORD = 'luzdedragonpolicepalo'
+EMAIL_HOST_USER = 'xxxxx@gmail.com'
+EMAIL_HOST_PASSWORD = 'xxxxxx'
 
 CKEDITOR_CONFIGS = {
     'default': {
